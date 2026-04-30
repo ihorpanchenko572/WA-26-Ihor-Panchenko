a7141aac5234c87b4d77a2be7b3cecaa0dc55a0c
Ihor Panchenko <ihor.panchenko@tul.cz>
2026-04-30 14:21:48 +0200

    subcategory

--- BooksAppSkA/app/models/Category.php
+++ BooksAppSkA/app/models/Category.php
@@ -7,10 +7,19 @@ class Category {
         $this->db = $db;
     }
 
-    // Metoda pro získání všech kategorií seřazených podle názvu
+    // Tuhle metodu tam už máš
     public function getAllCategories() {
         $stmt = $this->db->prepare("SELECT * FROM categories ORDER BY name ASC");
         $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // !!! TUTO METODU TAM MUSÍŠ PŘIDAT !!!
+    public function getAllSubcategories() {
+        $sql = "SELECT * FROM subcategories ORDER BY name ASC";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 }
